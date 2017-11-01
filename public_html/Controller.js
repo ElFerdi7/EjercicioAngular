a1e6635e3d23bedcb6c17a93b835e8e716515025
Fernando <Fernando@192.168.1.70>
2017-11-01 11:34:07 +0100

    Cambiado a la tabla de usuario

--- public_html/Controller.js
+++ public_html/Controller.js
@@ -6,7 +6,7 @@
 
 
 
-var miModulo = angular.module("angular1", []).controller(
+/*var miModulo = angular.module("angular1", []).controller(
         'MiControlador', ['$scope', '$http',
             function ($scope, $http) {
                 $http({
@@ -22,5 +22,48 @@ var miModulo = angular.module("angular1", []).controller(
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
                 });
-            }])
+            }])*/
+
+var miModulo = angular.module("angular1", []);
+miModulo.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.withCredentials = true;
+    }]);
+miModulo.controller('MiControlador', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.mostrarUsuario = false;
+        $scope.mostrarTabla = false;
+        $scope.idusuario = 0;
+        $scope.pagina = 1;
+        $scope.traerUsuario = function () {
+            if ($scope.idusuario > 0) {
+                $http({
+                    method: 'GET',
+                    url: 'http://localhost:8081/ejercicioInterfaces/json?ob=usuario&op=get&id=' + $scope.idusuario
+                }).then(function successCallback(response) {
+                    if (response.data.status == 200) {
+                        $scope.oUsuario = response.data.json;
+                    } else {
+                        $scope.error_angular = "Error en la recepción de datos";
+                    }
+                }, function errorCallback(response) {
+                    $scope.error_angular = "Error en la recepción de datos";
+                });
+            }
+        }
+        $scope.traerTabla = function () {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/ejercicioInterfaces/json?ob=usuario&op=getpage&rpp=3&np=' + $scope.pagina
+            }).then(function successCallback(response) {
+                if (response.data.status == 200) {
+                    $scope.arrUsuarios = response.data.json;
+                } else {
+                    $scope.error_angular = "Error en la recepción de datos";
+                }
+            }, function errorCallback(response) {
+                $scope.error_angular = "Error en la recepción de datos";
+            });
+ 
+        }
+    }])
   
