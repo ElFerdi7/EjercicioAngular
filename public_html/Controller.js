91d6b4fc274c904a6b6ee246f17d0ec1a24b4b11
a022583952e <a022583952e@S09-PC14>
2017-11-02 08:21:03 +0100

    Añadido currency

--- public_html/Controller.js
+++ public_html/Controller.js
@@ -4,26 +4,6 @@
  * and open the template in the editor.
  */
 
-
-
-/*var miModulo = angular.module("angular1", []).controller(
-        'MiControlador', ['$scope', '$http',
-            function ($scope, $http) {
-                $http({
-                    method: 'GET',
-                    url: 'http://localhost:8081/ejercicioInterfaces/json?ob=producto&op=getpage&np=1&rpp=10'
-                }).then(function successCallback(response) {
-                    if (response.data.status == 200) {
-                        $scope.alberto = response.data.json;
-                    } else {
-                        $scope.error_angular = "Error en la recepción de datos";
-                    }
-                }, function errorCallback(response) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                });
-            }])*/
-
 var miModulo = angular.module("angular1", []);
 miModulo.config(['$httpProvider', function ($httpProvider) {
         $httpProvider.defaults.withCredentials = true;
